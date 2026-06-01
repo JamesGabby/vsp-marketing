b34f74ecea04f5c44cd2ee86f113d40eb0b3def6
JamesGabby <jlghfe36@gmail.com>
2026-06-01 19:48:59 +0100

    add vsl

--- app/page.tsx
+++ app/page.tsx
@@ -11,7 +11,7 @@ export default function HomePage() {
     <>
       <Hero />
       <MetricsBar />
-      {/* <VSLVideo /> */}
+      <VSLVideo />
       <ServicesGrid />
       <ProcessTimeline />
       <Differentiators />
